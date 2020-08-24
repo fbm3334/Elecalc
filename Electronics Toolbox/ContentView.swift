3392a4982c496ff3e1be2c19468f7feade81a7ed
Finn Beckitt-Marshall <finn@pictura.co.uk>
2020-08-24 14:03:51 +0100

    Initial Commit

---
+++ Electronics Toolbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Electronics Toolbox
+//
+//  Created by Finn Beckitt-Marshall on 24/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
