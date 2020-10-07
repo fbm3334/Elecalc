70f34fc251b5c3eb34c7420f144fe43d7cd21225
Finn Beckitt-Marshall <finn@pictura.co.uk>
2020-10-07 10:31:48 +0100

    Wrote LC resonance class/function

---
+++ Electronics Toolbox/Classes/LCResonanceCalc.swift
@@ -0,0 +1,25 @@
+//
+//  LCResonanceCalc.swift
+//  Elecalc
+//
+//  Created by Finn Beckitt-Marshall on 07/10/2020.
+//
+
+import Foundation
+
+// Class used to calculate the LC resonant frequency
+class LCResonanceCalc: ObservableObject {
+    var siPrefixCalc = SIPrefixCalc()
+    
+    // Function to calculate the LC resonant frequency
+    func calcLCResFreq(inductance: InductorValue, capacitance: CapacitorValue) -> FrequencyValue {
+        // Calc the internal inductance and capacitance
+        let inductanceInt = inductance.value * pow(10.0, Double(inductance.prefix.rawValue))
+        let capacitanceInt = capacitance.value * pow(10.0, Double(capacitance.prefix.rawValue))
+        // Calculate the frequency in Hz
+        let freq = 1.0 / (2 * Double.pi * sqrt(inductanceInt * capacitanceInt))
+        print(freq)
+        // Return the frequency
+        return siPrefixCalc.calcFrequencyPrefix(value: freq, prefix: .Hz)
+    }
+}
